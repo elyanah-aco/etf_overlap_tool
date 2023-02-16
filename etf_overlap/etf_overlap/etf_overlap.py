7ec360a05d16eef573a61d9bfff79cb933081e17
elyanah <elyanah.aco02@gmail.com>
2023-02-16 22:41:18 +0800

    Upload initial files and helper functions

---
+++ etf_overlap/etf_overlap/etf_overlap.py
@@ -0,0 +1,44 @@
+# type: ignore
+from pcconfig import config
+
+import pynecone as pc
+from openbb_terminal.sdk import openbb
+
+docs_url = "https://pynecone.io/docs/getting-started/introduction"
+filename = f"{config.app_name}/{config.app_name}.py"
+
+
+class State(pc.State):
+    """The app state."""
+    etf_1: str
+    etf_2: str
+
+    def 
+
+
+def index() -> pc.Component:
+    return pc.center(
+        pc.vstack(
+            pc.heading("Welcome to Pynecone!", font_size="2em"),
+            pc.box("Get started by editing ", pc.code(filename, font_size="1em")),
+            pc.link(
+                "Check out our docs!",
+                href=docs_url,
+                border="0.1em solid",
+                padding="0.5em",
+                border_radius="0.5em",
+                _hover={
+                    "color": "rgb(107,99,246)",
+                },
+            ),
+            spacing="1.5em",
+            font_size="2em",
+        ),
+        padding_top="10%",
+    )
+
+
+# Add state and page to the app.
+app = pc.App(state=State)
+app.add_page(index)
+app.compile()
---
+++ etf_overlap/etf_overlap/helper.py
@@ -0,0 +1,88 @@
+import numpy as np
+import pandas as pd
+
+from openbb_terminal.sdk import openbb
+
+class ETFHelpers:
+    """ Contains helper functions. """
+
+    def __init__(self, etf_1: str, etf_2: str):
+        self.etf_1 = etf_1
+        self.etf_2 = etf_2
+        self.merged_etfs = None
+        self.greater = self.lesser = None
+
+    def set_merged_etfs(self) -> None:
+        """ Merge holdings data into one dataframe. """
+
+        try:
+            df_1 = openbb.etf.holdings(self.etf_1)
+            df_2 = openbb.etf.holdings(self.etf_2)
+            df_1 = self.clean_holdings(df_1)
+            df_2 = self.clean_holdings(df_2)
+
+            merged = df_1.merge(
+                df_2,
+                how='outer',
+                on=['symbol', 'name'],
+                suffixes=(f'_{self.etf_1}', f'_{self.etf_2}'),
+                indicator=True
+                )
+
+            self.merged_etfs = merged
+
+        except ValueError:
+            raise ValueError(
+                """
+                ETF data not found for at least one symbol.
+                Maybe you misspelled a symbol?
+                """)
+            
+
+    def set_etf_size(self) -> None:
+        """
+        Determine which ETF is "lesser" or "greater" based on
+        total shares.
+
+        Greater ETF = ETF with higher total shares
+        Lesser ETF = ETF with lower total shares
+        """
+        merged_etfs = self.merged_etfs
+        etf_1_total = merged_etfs[f'shares_{self.etf_1}'].sum()
+        etf_2_total = merged_etfs[f'shares_{self.etf_2}'].sum()
+        self.greater = self.etf_1 if etf_1_total > etf_2_total else self.etf_2
+        self.lesser = self.etf_1 if etf_1_total < etf_2_total else self.etf_2
+        
+    def get_lesser_overlap(self) -> float:
+        """
+        Get percentage of the lesser ETF that overlaps with the greater ETF. 
+        """
+        merged_etfs = self.merged_etfs
+        overlap = merged_etfs[merged_etfs['_merge'] == 'both']
+        return round(overlap[f'percent_{self.lesser}'].sum(), 2)
+
+    def get_greater_overlap(self) -> float:
+        merged_etfs = self.merged_etfs
+        overlap = merged_etfs[merged_etfs['_merge'] == 'both']
+        return round(overlap[f'percent_{self.greater}'].sum(), 2)
+
+    def get_lesser_top_overlaps(self, n: int = 10) -> pd.DataFrame:
+        """
+        Get top n overlapping holdings in the lesser stock
+        based on percentages. 
+        """
+        lesser = self.lesser
+        merged_etfs = self.merged_etfs
+        overlap = merged_etfs[merged_etfs['_merge'] == 'both']
+        overlap = overlap.sort_values(f'percent_{lesser}', ascending=False)
+        overlap = overlap[['symbol', 'name', f'percent_{lesser}', f'shares_{lesser}']]
+        overlap.columns = ['symbol', 'name', 'percent', 'shares']
+        return overlap.head(n)
+
+    @staticmethod
+    def clean_holdings(df: pd.DataFrame) -> pd.DataFrame:
+        """ Fix dataframe formatting. """
+        df = df.reset_index()
+        df.columns = ['symbol', 'name', 'percent', 'shares']
+        df['percent'] = df['percent'].str.rstrip('%').astype(float) / 100
+        return df
